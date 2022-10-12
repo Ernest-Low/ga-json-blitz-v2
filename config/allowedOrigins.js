a273482875364936bd36b0cddfeeb91f2b10e906
Ernest Low <ernestlow32@gmail.com>
2022-10-12 23:54:37 +0800

    added cyclic URL to CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   //* Add deployment website url when deploying
   "http://localhost:5173",
   "http://localhost:3100",
+  "https://zany-eel-buckle.cyclic.app",
 ];
 
 module.exports = allowedOrigins;
